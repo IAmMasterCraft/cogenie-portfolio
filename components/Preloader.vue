cc967e98f5defa627e7403643341582e97814450
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-12-13 22:38:03 +0100

    1st attempt to fix preloader problem in production

--- components/Preloader.vue
+++ components/Preloader.vue
@@ -13,3 +13,34 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        clPreloader () {
+            $("html").addClass('cl-preload');
+
+            $(window).on('load', function() {
+
+                //force page scroll position to top at page refresh
+                // $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+                // will first fade out the loading animation 
+                $("#loader").fadeOut("slow", function() {
+                    // will fade out the whole DIV that covers the website.
+                    $("#preloader").delay(300).fadeOut("slow");
+                }); 
+                
+                // for hero content animations 
+                $("html").removeClass('cl-preload');
+                $("html").addClass('cl-loaded');
+            
+            });
+        }, // end
+    },
+
+    mounted() {
+        this.clPreloader();
+    },
+}
+</script>
