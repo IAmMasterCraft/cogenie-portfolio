e7f50140feb647c174eea669bee2840a5c5284c8
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-12-13 05:39:44 +0100

    assets added

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,15 +7,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'cogenie',
+    title: 'Cogenie',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Portfolio for Cogenie' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '~/assets/css/base.css' },
+      { rel: 'stylesheet', href: '~/assets/css/vendor.css' },
+      { rel: 'stylesheet', href: '~/assets/css/main.css' }
     ]
   },
 
